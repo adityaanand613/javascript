814eb7ebf92bcb263e8e7d41d52eb555fafaf498
Aditya Anand <adityaanand613@gmail.com>
2024-08-14 22:26:41 +0530

    clock js file updated

--- clock.js
+++ clock.js
@@ -2,6 +2,7 @@ const hourEl=document.getElementById("hour");
 const minuteEl=document.getElementById("minute");
 const secondEl=document.getElementById("second");
 const ampmEl=document.getElementById("ampm");
+const bodyEl=document.body;
 
 function updateClock(){
     let h=new Date().getHours();
@@ -14,6 +15,16 @@ function updateClock(){
         h=h-12;
         ampm="PM";
     }
+    else if(h === 12)
+    {
+        ampm="PM"
+    }
+    else if(h === 0)
+    {
+        h=12;
+    }
+
+    // Adding leading zeroes
     h = h < 10 ? "0" + h : h;
     m = m < 10 ? "0" + m : m;
     s = s < 10 ? "0" + s : s;
@@ -22,6 +33,18 @@ function updateClock(){
     minuteEl.innerText = m;
     secondEl.innerText = s;
     ampmEl.innerText = ampm;
+
+    // Setting dynamic background based on time of day
+    if (h >= 5 && h < 12 && ampm === "AM") {
+        bodyEl.style.backgroundImage = "url('https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?fit=crop&w=1920&q=80')";
+    } else if (h >= 12 && h < 5 && ampm === "PM") {
+        bodyEl.style.backgroundImage = "url('https://images.unsplash.com/photo-1525357816819-392d2385a452?fit=crop&w=1920&q=80')";
+    } else if (h >= 5 && h < 8 && ampm === "PM") {
+        bodyEl.style.backgroundImage = "url('https://images.unsplash.com/photo-1502082553048-f009c37129b9?fit=crop&w=1920&q=80')";
+    } else {
+        bodyEl.style.backgroundImage = "url('https://images.unsplash.com/photo-1483058712412-4245e9b90334?fit=crop&w=1920&q=80')";
+    }
+
     setTimeout(() => {
         updateClock();
     },1000);
